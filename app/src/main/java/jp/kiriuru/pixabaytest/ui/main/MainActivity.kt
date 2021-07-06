e0e81f5b498267a3e2209ce2b5329bf87ab2be91
shulepov2 <shulepov2@gmail.com>
2021-07-06 21:42:04 +0500

    Build 1.3 Replace livedata in MainActivity an coroutine sharedFlow

--- app/src/main/java/jp/kiriuru/pixabaytest/ui/main/MainActivity.kt
+++ app/src/main/java/jp/kiriuru/pixabaytest/ui/main/MainActivity.kt
@@ -7,6 +7,9 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.GridLayoutManager
 import jp.kiriuru.pixabaytest.R
 import jp.kiriuru.pixabaytest.data.adapter.RvAdapter
@@ -16,6 +19,8 @@ import jp.kiriuru.pixabaytest.data.model.Hits
 import jp.kiriuru.pixabaytest.databinding.ActivityMainBinding
 import jp.kiriuru.pixabaytest.utils.Status
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
@@ -34,8 +39,8 @@ class MainActivity : AppCompatActivity() {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
         initRV()
-        searchImages(mViewModel.request)
 
+        searchImages(mViewModel.request)
         mBinding.btn.visibility = View.GONE
         mBinding.searchField.doAfterTextChanged {
             if (it != null) {
@@ -53,24 +58,31 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    //Coroutine+alpha lifecycle
     private fun searchImages(request: String) {
-        mViewModel.searchImage(request, defaultPerImage).observe(this, {
-            it?.let { resources ->
-                when (resources.status) {
-                    Status.SUCCESS -> {
-                        it.data?.let { item ->
-                            update(item.hits)
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                mViewModel.searchImage(request, defaultPerImage).collect {
+                    it.let { resource ->
+                        when (resource.status) {
+                            Status.SUCCESS -> {
+                                it.data?.let { image ->
+                                    update(image.hits)
+                                }
+                            }
+                            Status.ERROR -> {
+                                Toast.makeText(this@MainActivity, it.message, Toast.LENGTH_SHORT)
+                                    .show()
+                                Log.d(TAG, "${it.message}")
+                            }
+                            Status.LOADING -> {
+                            }
                         }
-                    }
-                    Status.ERROR -> {
-                        Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
-                        Log.d(TAG, "${it.message}")
-                    }
-                    Status.LOADING -> {
                     }
                 }
             }
-        })
+
+        }
     }
 
     private fun initRV() {
@@ -88,6 +100,38 @@ class MainActivity : AppCompatActivity() {
     private fun update(hits: List<Hits>) {
         mAdapter.addSource(hits = hits)
     }
+
+
+    //Coroutine
+//    private fun search2(request: String) {
+//        lifecycleScope.launchWhenStarted {
+//            mViewModel.search3(request, defaultPerImage).collect {
+//                update(it.hits)
+//            }
+//        }
+//    }
+
+
+    //livedata
+//    private fun search(request: String) {
+//        mViewModel.searchImage(request, defaultPerImage).observe(this, {
+//            it?.let { resources ->
+//                when (resources.status) {
+//                    Status.SUCCESS -> {
+//                        it.data?.let { item ->
+//                            update(item.hits)
+//                        }
+//                    }
+//                    Status.ERROR -> {
+//                        Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+//                        Log.d(TAG, "${it.message}")
+//                    }
+//                    Status.LOADING -> {
+//                    }
+//                }
+//            }
+//        })
+//    }
 
 }
 
--- app/src/main/java/jp/kiriuru/pixabaytest/ui/main/MainViewModel.kt
+++ app/src/main/java/jp/kiriuru/pixabaytest/ui/main/MainViewModel.kt
@@ -2,27 +2,57 @@ package jp.kiriuru.pixabaytest.ui.main
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
 import jp.kiriuru.pixabaytest.data.repository.ImageRepository
 import jp.kiriuru.pixabaytest.utils.Resource
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.shareIn
 
 const val TAG = "MAIN_VIEW_MODEL"
 
 
 class MainViewModel(private val repository: ImageRepository, var request: String) : ViewModel() {
 
-    fun searchImage(req: String, perPage: Int) = liveData(viewModelScope.coroutineContext) {
+    //SharedFlow + Resource
+    fun searchImage(req: String, perPage: Int) = flow {
         request = req
         Log.d(TAG, "Start")
         emit(Resource.loading(data = null))
         try {
+            Log.d(TAG, "Success")
             emit(Resource.success(data = repository.searchImage(request, perPage)))
-            Log.d(TAG, "Succses")
         } catch (exception: Exception) {
-            emit(Resource.error(data = null, message = exception.message ?: "Error Occurred!"))
+            emit(Resource.error(data = null, message = exception.message ?: "Error Occurred"))
         }
-    }
+    }.shareIn(viewModelScope, started = SharingStarted.Lazily, replay = 1)
+
+
+    //SharedFlow без Resource
+//    fun search3(req: String, perPage: Int) = flow {
+//        request = req
+//        emit(repository.searchImage(req, perPage))
+//    }.shareIn(viewModelScope, started = SharingStarted.Lazily, replay = 1)
+
+//    пример когда в  Api отдаем Flow<PixBay> убирая suspendы
+//
+//    suspend fun search(request: String, perPage: Int) {
+//        val image: SharedFlow<PixabayResponse> = repository.searchImage(request, perPage)
+//            .shareIn(viewModelScope, started = SharingStarted.Lazily, replay = 1)
+//    }
+
+    //livedata
+//    fun searchImage(req: String, perPage: Int) = liveData(viewModelScope.coroutineContext) {
+//        request = req
+//        Log.d(TAG, "Start")
+//        emit(Resource.loading(data = null))
+//        try {
+//            emit(Resource.success(data = repository.searchImage(request, perPage)))
+//            Log.d(TAG, "Succses")
+//        } catch (exception: Exception) {
+//            emit(Resource.error(data = null, message = exception.message ?: "Error Occurred!"))
+//        }
+//    }
 
 }
 
